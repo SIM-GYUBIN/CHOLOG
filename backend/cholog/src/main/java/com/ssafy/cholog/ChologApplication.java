c561469acd48e0388fe2ea61f17c2842df38792b
심규빈 <skb0516@naver.com>
2025-04-28 14:23:52 +0900

    init: 프로젝트 초기 생성
    S12P31B207-52

---
+++ backend/cholog/src/main/java/com/ssafy/cholog/ChologApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.cholog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class ChologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChologApplication.class, args);
+	}
+}
