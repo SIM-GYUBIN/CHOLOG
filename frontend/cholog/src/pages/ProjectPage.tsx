bf1dad7dfe1b6232500b1f037968745579e9b9d0
류현 <ryoo918@naver.com>
2025-05-07 17:23:48 +0900

    feat : copy기능연결 S12P31B207-86

--- frontend/cholog/src/pages/ProjectPage.tsx
+++ frontend/cholog/src/pages/ProjectPage.tsx
@@ -1,13 +1,36 @@
 import { useParams } from 'react-router-dom';
 import LogList from '../components/logList';
+import copy from '@/assets/copy.svg';
+import report from '@/assets/report.svg';
+import setting from '@/assets/setting.svg';
 
 const ProjectPage = () => {
   const { projectId } = useParams();
+  const projectName = "프로젝트이름이다";
+  const handleCopyClipBoard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("프로젝트ID 복사 완료");
+    } catch (e) {
+      alert("프로젝트ID 복사 실패");
+    }
+  };
 
   return (
-    <div>
-      Project Page
-      <p>Project ID: {projectId}</p>
+    <div className='flex flex-col max-w-[60vw] mx-auto '>
+      <div className='flex flex-row items-center gap-2 px-5 font-[paperlogy5]'>
+        <div className=' text-[24px] text-slate-500'>{projectName}</div>
+        <div className='text-[20px] text-slate-300'>{projectId}</div>
+        <div className='rounded-sm p-1 hover:bg-gray-200' onClick={() => handleCopyClipBoard(projectId || '')}>
+          <img src={copy} alt="복사" className='w-5 h-5' />
+        </div>
+        <div className='rounded-sm p-1 hover:bg-gray-200'>
+          <img src={report} alt="복사" className='w-5 h-5' />
+        </div>
+        <div className='rounded-sm p-1 hover:bg-gray-200'>
+          <img src={setting} alt="복사" className='w-5 h-5' />
+        </div>
+      </div>
       <LogList />
     </div>
   );
