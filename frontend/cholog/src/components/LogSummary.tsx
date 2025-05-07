dc57050aca3f7bce4c7fe1ec8fa7f9a35c67a2dd
2m7r <2m7r.sw@gmail.com>
2025-05-07 16:45:18 +0900

    feat : 프로젝트 로그 통계 컴포넌트 제작
    - 프로젝트 로그 통계 컴포넌트 제작
    - 시간별 로그 추이 그래프와 디자인 통일
    
    S12P31B207-82

---
+++ frontend/cholog/src/components/LogSummary.tsx
@@ -0,0 +1,62 @@
+import errorIcon from "@/assets/levelicon/error.png";
+import warnIcon from "@/assets/levelicon/warn.png";
+import infoIcon from "@/assets/levelicon/info.png";
+import debugIcon from "@/assets/levelicon/debug.png";
+import traceIcon from "@/assets/levelicon/trace.png";
+import fatalIcon from "@/assets/levelicon/fatal.png";
+
+const LogSummary = () => {
+  const logsMockData = {
+    total: 2183,
+    logs: {
+      error: 56,
+      warn: 21,
+      info: 1240,
+      debug: 5,
+      trace: 861,
+      fatal: 0,
+    },
+  };
+
+  const ICONS = {
+    error: errorIcon,
+    warn: warnIcon,
+    info: infoIcon,
+    debug: debugIcon,
+    trace: traceIcon,
+    fatal: fatalIcon,
+  };
+
+  const LABELS = ["error", "debug", "warn", "trace", "info", "fatal"] as const;
+
+  return (
+    <div className="rounded-2xl border border-gray-200 p-8 shadow-md w-fit bg-white">
+      <div className="text-left text-gray-800 mb-4 flex items-end gap-3">
+        <span className="text-lg">Total</span>
+        <span className="text-2xl font-bold">
+          {logsMockData.total.toLocaleString()}
+        </span>
+      </div>
+
+      <div className="grid grid-cols-2 gap-x-18 gap-y-6">
+        {LABELS.map((label) => (
+          <div key={label} className="flex items-center gap-3">
+            <img
+              src={ICONS[label]}
+              alt={`${label} icon`}
+              className="w-10 h-10"
+            />
+            <div className="flex flex-col text-left text-gray-800">
+              <span className="text-sm text-gray-800">{label}</span>
+              <span className="text-base font-medium">
+                {logsMockData.logs[label]}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LogSummary;
