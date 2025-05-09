f76ade9bf85155f553e2aa9373a4b33535598479
2m7r <2m7r.sw@gmail.com>
2025-05-09 11:10:04 +0900

    fix : 디자인 수정
    - 아이콘 색상 및 크기 수정
    - 다크모드 적용 위한 디자인 수정
    S12P31B207-95

--- frontend/cholog/src/components/ProjectList/ProjectActions.tsx
+++ frontend/cholog/src/components/ProjectList/ProjectActions.tsx
@@ -8,13 +8,13 @@ const ProjectActions = ({ onAdd, onJoin }: ProjectActionsProps) => {
     <div className="flex justify-start gap-4 mb-8">
       <button
         onClick={onAdd}
-        className="px-6 py-2 bg-[#F8FAFC] text-[#64748B] border border-[#CBD5E1] rounded-2xl hover:bg-gray-50 transition-colors font-paperlogy5 cursor-pointer"
+        className="px-6 py-2 bg-[[var(--bg)]] text-[var(--helpertext)] border border-[var(--line)] rounded-2xl hover:bg-gray-50 transition-colors font-paperlogy5 cursor-pointer"
       >
         ADD
       </button>
       <button
         onClick={onJoin}
-        className="px-6 py-2 bg-[#F8FAFC] text-[#64748B] border border-[#CBD5E1] rounded-2xl hover:bg-gray-50 transition-colors font-paperlogy5 cursor-pointer"
+        className="px-6 py-2 bg-[[var(--bg)]] text-[var(--helpertext)] border border-[var(--line)] rounded-2xl hover:bg-gray-50 transition-colors font-paperlogy5 cursor-pointer"
       >
         JOIN
       </button>
