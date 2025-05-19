f40d322dbc453a0672c825b6c22bb59e6a8c3883
심규빈 <skb0516@naver.com>
2025-05-19 13:01:39 +0900

    refactor: 일반 로그인 ok로 변신

--- backend/cholog/src/main/java/com/ssafy/cholog/domain/user/controller/UserController.java
+++ backend/cholog/src/main/java/com/ssafy/cholog/domain/user/controller/UserController.java
@@ -42,7 +42,8 @@ public class UserController {
         ResponseCookie accessTokenCookie = CookieUtil.makeAccessTokenCookie(loginResult.getAccessToken());
         ResponseCookie refreshTokenCookie = CookieUtil.makeRefreshTokenCookie(loginResult.getRefreshToken());
 
-        return CommonResponse.redirectWithCookie("https://www.cholog.com", accessTokenCookie, refreshTokenCookie);
+        return CommonResponse.okWithCookie(accessTokenCookie, refreshTokenCookie);
+//        return CommonResponse.redirectWithCookie("https://www.cholog.com", accessTokenCookie, refreshTokenCookie);
     }
 
     @GetMapping("/login/{provider}")
