acff36c2acd6fac579f55c554828c55379b862bb
심규빈 <skb0516@naver.com>
2025-05-19 14:08:36 +0900

    feat: cholog 설치

--- frontend/cholog/src/main.tsx
+++ frontend/cholog/src/main.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store/store';  // store 경로 정확히 지정
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/store"; // store 경로 정확히 지정
+import App from "./App";
+import "./index.css";
+import Cholog from "cholog-sdk";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+Cholog.init({
+  projectKey: "f98c78f3-8bea-4ead-a631-3d79bae44c11",
+  environment: "prod",
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
