e2c4a77c0468461e5c380ac7bb461c96c919d6d7
이은지 <dmswldk28@gmail.com>
2025-05-21 17:45:48 +0900

    fix: 랜딩페이지 코드 수정

--- frontend/cholog/src/main.tsx
+++ frontend/cholog/src/main.tsx
@@ -4,12 +4,12 @@ import { Provider } from "react-redux";
 import store from "./store/store"; // store 경로 정확히 지정
 import App from "./App";
 import "./index.css";
-// import Cholog from "cholog-sdk";
+import Cholog from "cholog-sdk";
 
-// Cholog.init({
-//   apiKey: "f98c78f3-8bea-4ead-a631-3d79bae44c11",
-//   environment: "prod",
-// });
+Cholog.init({
+  apiKey: "f98c78f3-8bea-4ead-a631-3d79bae44c11",
+  environment: "prod",
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
